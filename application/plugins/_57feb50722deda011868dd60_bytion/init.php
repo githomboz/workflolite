0ced6a6b5c8cab432d11f071fc942fc2bf34ce8d
Jahdy Lancelot <jahdy@spotflare.com>
2016-11-21 13:07:32 -0500

    Final commit under the assumption of partner. Client wants their own project first which simply classifies them as a client and makes this a magnified project management app and not really a workflow product as originally intended under the joined effort and means for partners JNBPA and CS.

---
+++ application/plugins/_57feb50722deda011868dd60_bytion/init.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: benezerlancelot
+ * Date: 11/12/16
+ * Time: 10:12 AM
+ */
+
+
+class BYTION {
+
+  public static function ProcessIncomingOrder($event, $payload){
+
+  }
+
+}
